2d80802477be16dda0ca055bf4b914cd6edbe27f
LostLemonCurd <lounis@MacBook-Air-de-Lounis.local>
2025-06-06 10:22:43 +0200

    feat(navigation-router): added basic navigation and routing.

---
+++ app/(tabs)/folio.tsx
@@ -0,0 +1,22 @@
+import { Text, View, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Folio() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Folio screen</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
