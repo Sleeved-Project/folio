78b925c26dcd01870b67dbc348ad6999a45b3b17
MaximePETIT-code <contactmaximepetit@gmail.com>
2025-06-06 11:10:32 +0200

    fix(husky): simplify pre-commit hook to run checks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
   root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,4 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
+import { Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
