3665f05aad6f5f953cae2fc60e4b9c4bbca4555c
LostLemonCurd <lounis@MacBook-Air-de-Lounis.local>
2025-06-09 18:29:57 +0200

    fix(search-bar): added debounce to the search bar and removed the ? from the parameters

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,19 +1,39 @@
 import { Search } from 'lucide-react-native';
+import { useEffect, useRef, useState } from 'react';
 import { TextInput, View, StyleSheet } from 'react-native';
+import { debounce } from 'lodash';
 
 interface SearchBarProps {
-  searchQuery?: string;
-  setSearchQuery?: (query: string) => void;
+  searchQuery: string;
+  setSearchQuery: (query: string) => void;
 }
 
 export default function SearchBar({ searchQuery, setSearchQuery }: SearchBarProps) {
+  const [inputValue, setInputValue] = useState(searchQuery);
+
+  // We are debouncing the query to avoid too many updates, it will only be sent after 1 second of inactivity
+  const debouncedSetSearchQuery = useRef(
+    debounce((query: string) => setSearchQuery(query), 1000)
+  ).current;
+
+  useEffect(() => {
+    debouncedSetSearchQuery(inputValue);
+    return () => {
+      debouncedSetSearchQuery.cancel();
+    };
+  }, [inputValue, debouncedSetSearchQuery]);
+
+  useEffect(() => {
+    setInputValue(searchQuery);
+  }, [searchQuery]);
+
   return (
     <View style={styles.container}>
       <Search color={'#A09CAB'} />
       <TextInput
         style={styles.input}
-        value={searchQuery}
-        onChangeText={setSearchQuery}
+        value={inputValue}
+        onChangeText={setInputValue}
         placeholder="Search"
         placeholderTextColor={'#A09CAB'}
       />
