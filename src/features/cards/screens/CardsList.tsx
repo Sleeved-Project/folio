abe3c0f144da0bc1e0e5fdc4e8f269b19eddcc96
LostLemonCurd <109440184+LostLemonCurd@users.noreply.github.com>
2025-06-28 12:50:48 +0200

    feat(sets): display list of sets

--- src/features/cards/screens/CardsList.tsx
+++ src/features/cards/screens/CardsList.tsx
@@ -4,15 +4,41 @@ import { useState } from 'react';
 import SearchBar from '../../../components/ui/SearchBar';
 import CardListDisplay from '../components/CardListDisplay';
 import { useTheme } from '../../../theme/useTheme';
+import { TabOption, TabSwitcher } from '../../../components/ui/TabSwitcher';
+import CardSetDisplay from '../components/CardSetDisplay';
+import { useSets } from '../hooks/queries/useSetsQuery';
+
+type TabType = 'sets' | 'cards';
 
 export default function CardsList() {
   const [cardName, setCardName] = useState<string>('');
+  const [activeTab, setActiveTab] = useState<TabType>('sets');
   const theme = useTheme();
 
-  const { data, isLoading, error, fetchNextPage, hasNextPage, isFetchingNextPage } =
-    useCards(cardName);
+  const {
+    data: cardsData,
+    isLoading: isCardsLoading,
+    error: cardsError,
+    fetchNextPage: fetchNextCardsPage,
+    hasNextPage: hasNextCardsPage,
+    isFetchingNextPage: isFetchingNextCardsPage,
+  } = useCards(cardName);
 
-  const cards = data?.pages.flatMap((page) => page.data) ?? [];
+  const {
+    data: setsData,
+    isLoading: isSetsLoading,
+    error: setsError,
+    fetchNextPage: fetchNextSetsPage,
+    hasNextPage: hasNextSetsPage,
+    isFetchingNextPage: isFetchingNextSetsPage,
+  } = useSets(cardName);
+
+  const cards = cardsData?.pages.flatMap((page) => page.data) ?? [];
+  const sets = setsData?.pages.flatMap((page) => page.data) ?? [];
+  const tabOptions: TabOption<TabType>[] = [
+    { id: 'sets', label: 'Card Sets' },
+    { id: 'cards', label: 'All Cards' },
+  ];
 
   return (
     <View
@@ -25,18 +51,35 @@ export default function CardsList() {
         },
       ]}
     >
+      <TabSwitcher
+        options={tabOptions}
+        activeTabId={activeTab}
+        onTabChange={(tabId) => setActiveTab(tabId)}
+        containerStyle={{ marginTop: 8 }}
+      />
       <SearchBar
         searchQuery={cardName}
         setSearchQuery={(newName: string) => setCardName(newName)}
       />
-      <CardListDisplay
-        cards={cards}
-        hasNextPage={hasNextPage}
-        isFetchingNextPage={isFetchingNextPage}
-        isLoading={isLoading}
-        fetchNextPage={fetchNextPage}
-        error={error}
-      />
+      {activeTab === 'sets' ? (
+        <CardSetDisplay
+          sets={sets}
+          hasNextPage={hasNextSetsPage}
+          isFetchingNextPage={isFetchingNextSetsPage}
+          isLoading={isSetsLoading}
+          fetchNextPage={fetchNextSetsPage}
+          error={setsError}
+        />
+      ) : (
+        <CardListDisplay
+          cards={cards}
+          hasNextPage={hasNextCardsPage}
+          isFetchingNextPage={isFetchingNextCardsPage}
+          isLoading={isCardsLoading}
+          fetchNextPage={fetchNextCardsPage}
+          error={cardsError}
+        />
+      )}
     </View>
   );
 }
