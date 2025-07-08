9cb228cddf48741802851d89c96f645f24ebc347
LostLemonCurd <109440184+LostLemonCurd@users.noreply.github.com>
2025-07-08 20:03:59 +0200

    feat(filters): filter options are now fetched from the back end

--- src/context/FilterContext.tsx
+++ src/context/FilterContext.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useState, ReactNode } from 'react';
 import { FilterOption, Filters } from '../features/filters/types';
+import { useFilters } from '../features/filters/hooks/queries/useFiltersQuery';
 
 type FilterContextType = {
   filtersOptions: FilterOption;
@@ -23,97 +24,7 @@ export function FilterProvider({ children }: { children: ReactNode }) {
     {} as FilterOption
   );
 
-  // Need to replace to get the actual filters
-  const filtersOptions: FilterOption = {
-    artists: [
-      {
-        id: 20,
-        name: '"Big Mama" Tagawa',
-      },
-      {
-        id: 169,
-        name: '"Big Mama" Tagawa & Benimaru Itoh',
-      },
-      {
-        id: 164,
-        name: '"Big Mama" Tagawa, CR CG gangs',
-      },
-      {
-        id: 195,
-        name: '0313',
-      },
-      {
-        id: 345,
-        name: 'tono',
-      },
-      {
-        id: 275,
-        name: 'toriyufu',
-      },
-      {
-        id: 15,
-        name: 'Toshinao Aoki',
-      },
-    ],
-    rarities: [
-      {
-        id: 33,
-        label: 'ACE SPEC Rare',
-      },
-      {
-        id: 35,
-        label: 'Amazing Rare',
-      },
-      {
-        id: 13,
-        label: 'Classic Collection',
-      },
-      {
-        id: 5,
-        label: 'Common',
-      },
-      {
-        id: 26,
-        label: 'Double Rare',
-      },
-    ],
-    subtypes: [
-      {
-        id: 14,
-        label: 'ACE SPEC',
-      },
-      {
-        id: 32,
-        label: 'Ancient',
-      },
-      {
-        id: 6,
-        label: 'Baby',
-      },
-      {
-        id: 2,
-        label: 'Basic',
-      },
-    ],
-    types: [
-      {
-        id: 6,
-        label: 'Colorless',
-      },
-      {
-        id: 8,
-        label: 'Metal',
-      },
-      {
-        id: 1,
-        label: 'Psychic',
-      },
-      {
-        id: 4,
-        label: 'Water',
-      },
-    ],
-  };
+  const { data: filtersOptions } = useFilters();
 
   return (
     <FilterContext.Provider
@@ -122,7 +33,7 @@ export function FilterProvider({ children }: { children: ReactNode }) {
         setFilters,
         selectedFilterOption,
         setSelectedFilterOption,
-        filtersOptions,
+        filtersOptions: filtersOptions || ({} as FilterOption),
       }}
     >
       {children}
