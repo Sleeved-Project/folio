8348330bfe01db00eedefb864957f1ca921ec999
MaximePETIT-code <contactmaximepetit@gmail.com>
2025-08-05 00:32:52 +0200

    feat(tabs): marketplace & profile tab

--- src/features/cards/screens/CardsList.tsx
+++ src/features/cards/screens/CardsList.tsx
@@ -21,7 +21,7 @@ interface CardsListProps {
 
 export default function CardsList({ isFiltersVisible = false }: CardsListProps) {
   const [cardName, setCardName] = useState<string>('');
-  const [activeTab, setActiveTab] = useState<TabType>('cards');
+  const [activeTab, setActiveTab] = useState<TabType>('sets');
   const theme = useTheme();
   const { cardFilters, filtersOptions, setSelectedFilterOption } = useFilterContext();
 
