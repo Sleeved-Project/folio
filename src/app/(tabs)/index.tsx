1ce4a1dcc55974b89ba2894b225d2a447288deff
MaximePETIT-code <contactmaximepetit@gmail.com>
2025-08-05 10:53:30 +0200

    revert: feat(tabs): marketplace & profile tab

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { useTheme } from '../../theme/useTheme';
+import CardsList from '../../features/cards/screens/CardsList';
 
-export default function Index() {
+export default function Explorer() {
   const theme = useTheme();
 
   return (
     <View style={[styles.container, { backgroundColor: theme.colors.background.primary }]}>
       <View style={[styles.content, { backgroundColor: theme.colors.background.primary }]}>
-        <Text>Marketplace</Text>
+        <CardsList isFiltersVisible={true} />
       </View>
     </View>
   );
--- src/components/ui/TabHeader.tsx
+++ src/components/ui/TabHeader.tsx
@@ -1,5 +1,6 @@
 import { Text, StyleSheet, View } from 'react-native';
 import BackButton from './BackButton';
+import UserMenu from '../../features/auth/components/UserMenu';
 import { useTheme } from '../../theme/useTheme';
 
 interface TabHeaderProps {
@@ -13,22 +14,25 @@ export default function TabHeader({ title, displayBackButton }: TabHeaderProps) 
   return (
     <View style={styles.container}>
       <View style={styles.side}>{displayBackButton && <BackButton />}</View>
-      {title && (
-        <Text
-          numberOfLines={1}
-          ellipsizeMode="tail"
-          style={[
-            styles.text,
-            {
-              color: theme.colors.text.primary,
-              fontSize: theme.typography.fontSizes.lg,
-            },
-          ]}
-        >
-          {title}
-        </Text>
-      )}
-      <View style={styles.side} />
+      <View style={styles.center}>
+        {title && (
+          <Text
+            style={[
+              styles.text,
+              {
+                color: theme.colors.text.primary,
+                backgroundColor: theme.colors.background.primary,
+                fontSize: theme.typography.fontSizes.lg,
+              },
+            ]}
+          >
+            {title}
+          </Text>
+        )}
+      </View>
+      <View style={styles.side}>
+        <UserMenu />
+      </View>
     </View>
   );
 }
@@ -39,23 +43,18 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    height: 44,
-    position: 'relative',
   },
   text: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    textAlign: 'center',
-    width: '100%',
-    zIndex: 0,
-    paddingHorizontal: 50,
-    fontWeight: '500',
+    alignSelf: 'center',
   },
   side: {
     width: 50,
     alignItems: 'center',
     justifyContent: 'center',
-    zIndex: 1,
+  },
+  center: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
