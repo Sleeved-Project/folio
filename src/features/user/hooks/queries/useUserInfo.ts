29bc374bc693f9ce15b00d99edbf17d1bd5f34fe
MaximePETIT-code <contactmaximepetit@gmail.com>
2025-08-07 00:47:58 +0200

    feat(profile): add user information query

---
+++ src/features/user/hooks/queries/useUserInfo.ts
@@ -0,0 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+import { httpClient } from '../../../../lib/client/http-client';
+
+export interface UserProfileData {
+  id: string;
+  username: string;
+  firstname: string | null;
+  lastname: string | null;
+  profilePictureUrl: string | null;
+  phone: string | null;
+  description: string | null;
+}
+
+export const userProfileKeys = {
+  all: ['userProfile'] as const,
+  details: (userId: string) => [...userProfileKeys.all, userId] as const,
+};
+
+export function useUserProfile(userId?: string) {
+  return useQuery<UserProfileData>({
+    queryKey: userProfileKeys.details(userId as string),
+    queryFn: async () => {
+      const uri = userId ? `/user/${userId}` : '/user';
+
+      const response = await httpClient.get<UserProfileData>(uri);
+      return response;
+    },
+    staleTime: 5 * 60 * 1000,
+  });
+}
