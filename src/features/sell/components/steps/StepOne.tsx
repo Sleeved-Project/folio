9715d53a02cabcbad4eb1c323b64d5bb1c977209
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2025-08-06 11:01:15 +0200

    feat(layout): adding last style handle on form

--- src/features/sell/components/steps/StepOne.tsx
+++ src/features/sell/components/steps/StepOne.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { View, ScrollView, KeyboardAvoidingView, Platform, StyleSheet } from 'react-native';
 import { SellFormAction } from '../../types';
 import { stepOneSchema, StepOneFormData, SellFormData } from '../../schemas/sellFormSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useTheme } from '../../../../theme/useTheme';
-import { FormTextInput, Button } from '../../../../components/ui';
+import { FormTextInput } from '../../../../components/ui';
 import StepHeader from './StepHeader';
+import StepLayout from './StepLayout';
 
 interface StepOneProps {
   dispatch: React.Dispatch<SellFormAction>;
@@ -14,8 +13,6 @@ interface StepOneProps {
 }
 
 export default function StepOne({ dispatch, defaultValues }: StepOneProps) {
-  const theme = useTheme();
-
   const {
     control,
     handleSubmit,
@@ -23,6 +20,7 @@ export default function StepOne({ dispatch, defaultValues }: StepOneProps) {
   } = useForm<StepOneFormData>({
     resolver: zodResolver(stepOneSchema),
     defaultValues: { name: defaultValues?.name || '' },
+    mode: 'onChange',
   });
 
   const onSubmit = (data: StepOneFormData) => {
@@ -31,95 +29,77 @@ export default function StepOne({ dispatch, defaultValues }: StepOneProps) {
   };
 
   return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior="padding"
-      keyboardVerticalOffset={Platform.OS === 'ios' ? -56 : -56}
-    >
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: theme.spacing.xl }}
-      >
-        <StepHeader
-          title="Exemple title"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit ipsum dolor sit amet."
-          infoField="* Required fields"
-        />
+    <StepLayout onNext={handleSubmit(onSubmit)} showPrevButton={true}>
+      <StepHeader
+        title="Exemple title"
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit ipsum dolor sit amet."
+        infoField="* Required fields"
+      />
 
-        <FormTextInput
-          control={control}
-          name="name"
-          isRequired
-          label="Your name "
-          placeholder="Enter your name"
-          error={errors.name?.message}
-        />
-        <FormTextInput
-          control={control}
-          name="name"
-          isRequired
-          label="Your name "
-          placeholder="Enter your name"
-          error={errors.name?.message}
-        />
-        <FormTextInput
-          control={control}
-          name="name"
-          isRequired
-          label="Your name "
-          placeholder="Enter your name"
-          error={errors.name?.message}
-        />
-        <FormTextInput
-          control={control}
-          name="name"
-          isRequired
-          label="Your name "
-          placeholder="Enter your name"
-          error={errors.name?.message}
-        />
-        <FormTextInput
-          control={control}
-          name="name"
-          isRequired
-          label="Your name "
-          placeholder="Enter your name"
-          error={errors.name?.message}
-        />
-        <FormTextInput
-          control={control}
-          name="name"
-          isRequired
-          label="Your name "
-          placeholder="Enter your name"
-          error={errors.name?.message}
-        />
-        <FormTextInput
-          control={control}
-          name="name"
-          isRequired
-          label="Your name "
-          placeholder="Enter your name"
-          error={errors.name?.message}
-        />
-        <FormTextInput
-          control={control}
-          name="name"
-          isRequired
-          label="Your name "
-          placeholder="Enter your name"
-          error={errors.name?.message}
-        />
-      </ScrollView>
-      <View style={{ marginTop: theme.spacing.md }}>
-        <Button title="Suivant" onPress={handleSubmit(onSubmit)} />
-      </View>
-    </KeyboardAvoidingView>
+      <FormTextInput
+        control={control}
+        name="name"
+        isRequired
+        label="Your name "
+        placeholder="Enter your name"
+        error={errors.name?.message}
+      />
+      <FormTextInput
+        control={control}
+        name="name"
+        isRequired
+        label="Your name "
+        placeholder="Enter your name"
+        error={errors.name?.message}
+      />
+      <FormTextInput
+        control={control}
+        name="name"
+        isRequired
+        label="Your name "
+        placeholder="Enter your name"
+        error={errors.name?.message}
+      />
+      <FormTextInput
+        control={control}
+        name="name"
+        isRequired
+        label="Your name "
+        placeholder="Enter your name"
+        error={errors.name?.message}
+      />
+      <FormTextInput
+        control={control}
+        name="name"
+        isRequired
+        label="Your name "
+        placeholder="Enter your name"
+        error={errors.name?.message}
+      />
+      <FormTextInput
+        control={control}
+        name="name"
+        isRequired
+        label="Your name "
+        placeholder="Enter your name"
+        error={errors.name?.message}
+      />
+      <FormTextInput
+        control={control}
+        name="name"
+        isRequired
+        label="Your name "
+        placeholder="Enter your name"
+        error={errors.name?.message}
+      />
+      <FormTextInput
+        control={control}
+        name="name"
+        isRequired
+        label="Your name "
+        placeholder="Enter your name"
+        error={errors.name?.message}
+      />
+    </StepLayout>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
