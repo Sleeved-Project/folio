894fd2dcc51e656a2093a20ff9eb2cbea70d1f80
LostLemonCurd <109440184+LostLemonCurd@users.noreply.github.com>
2025-09-05 18:53:54 +0200

    feat(stripe): init of connexion page

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { router } from 'expo-router';
+// import { router } from 'expo-router';
 import { StyleSheet, View } from 'react-native';
 import { Button } from '../../components/ui';
 import MarketplaceHome from '../../features/marketplace/screens/MarketplaceHome';
@@ -7,14 +7,28 @@ import { useTheme } from '../../theme/useTheme';
 export default function Marketplace() {
   const theme = useTheme();
 
-  const navigateToSellForm = () => {
-    router.push('/sell-form');
+  // const navigateToSellForm = () => {
+  //   router.push('/sell-form');
+  // };
+
+  const createStripeAccount = async () => {
+    try {
+      const stripeAccount = await fetch('http://192.168.0.33:8082/api/v1/payment/account', {
+        method: 'POST',
+      });
+      console.log('Stripe account created:', await stripeAccount.json());
+      // const { url } = await stripeAccount.json();
+      // Linking.openURL(url);
+    } catch (error) {
+      console.error('Error creating Stripe account:', error);
+    }
   };
 
   return (
     <View style={[styles.container, { backgroundColor: theme.colors.background.primary }]}>
       <View style={[styles.content, { backgroundColor: theme.colors.background.primary }]}>
-        <Button title="Sell a card" onPress={navigateToSellForm} />
+        <Button title="Create" onPress={createStripeAccount} />
+        {/* <Button title="Sell a card" onPress={navigateToSellForm} /> */}
         <MarketplaceHome />
       </View>
     </View>
