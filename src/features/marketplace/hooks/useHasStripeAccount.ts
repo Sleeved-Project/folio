8fc8166c970ed74c8898ad06ba22bec237a3db70
LostLemonCurd <109440184+LostLemonCurd@users.noreply.github.com>
2025-09-09 15:57:18 +0200

    misc(logs): deleted unused logs

--- src/features/marketplace/hooks/useHasStripeAccount.ts
+++ src/features/marketplace/hooks/useHasStripeAccount.ts
@@ -5,15 +5,8 @@ export const useHasStripeAccount = () => {
   return useQuery({
     queryKey: ['hasStripeAccount'],
     queryFn: async () => {
-      try {
-        const hasStripeAccount = await httpClient.get<boolean>('/user/stripe');
-        console.log('Fetched Stripe account status:', hasStripeAccount);
-
-        return hasStripeAccount;
-      } catch (error) {
-        console.error('Error fetching Stripe account status:', error);
-        return false;
-      }
+      const hasStripeAccount = await httpClient.get<boolean>('/user/stripe');
+      return hasStripeAccount;
     },
   });
 };
