e01eb25aa37f8372163e4a11521089117431d80b
MaximePETIT-code <contactmaximepetit@gmail.com>
2025-09-16 00:39:31 +0200

    fix(MarketplaceHome): improve layout and styling with safe area insets

--- src/features/marketplace/screens/MarketplaceHome.tsx
+++ src/features/marketplace/screens/MarketplaceHome.tsx
@@ -1,13 +1,15 @@
-import { ScrollView, View } from 'react-native';
+import { ScrollView, View, StyleSheet } from 'react-native';
 import CardsForSale from '../components/CardsForSale';
 import TopSellers from '../components/TopSellers';
 import { router } from 'expo-router';
 import { useHasStripeAccount } from '../hooks/useHasStripeAccount';
 import { useRefetchOnFocus } from '../../../hooks/useRefetchOnFocus';
 import { Button } from '../../../components/ui';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function MarketplaceHome() {
   const { data: hasStripeAccount, refetch: refetchHasStripeAccount } = useHasStripeAccount();
+  const insets = useSafeAreaInsets();
 
   useRefetchOnFocus(refetchHasStripeAccount);
 
@@ -20,12 +22,24 @@ export default function MarketplaceHome() {
   };
 
   return (
-    <View>
-      <Button title="Sell a card" onPress={navigateToStripeSetup} />
-      <ScrollView showsVerticalScrollIndicator={false}>
+    <View style={styles.container}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingBottom: insets.bottom,
+        }}
+      >
+        <Button title="Sell a card" onPress={navigateToStripeSetup} />
+
         <TopSellers />
         <CardsForSale />
       </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
